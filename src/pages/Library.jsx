2198a41dfa770c34f57dd0338e047295e89a32c1
VetsForAnimalWelfare <vfaw2017@gmail.com>
2026-09-03 08:52:56 +0545

    Update import path for Library.csscss upgrade

--- src/pages/Library.jsx
+++ src/pages/Library.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import { Link, useParams, useNavigate } from "react-router-dom";
-import "./Library.css";
+import "../Library.css";
 
 const blogs = [
   {
