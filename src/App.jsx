11ff34762f77b31254baf73c929cbeb0702d27ef
VetsForAnimalWelfare <vfaw2017@gmail.com>
2026-09-20 22:19:19 +0545

    Fix duplicate import and add Notice route

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ import Apply from './pages/Apply';
 import Gallery from './pages/Gallery';
 import GetInvolved from './pages/GetInvolved';
 import Contact from './pages/Contact';
+import Contact from './pages/Notice';
 import Donate from './pages/Donate';
 
 function App() {
@@ -27,6 +28,7 @@ function App() {
             <Route path="/gallery" element={<Gallery />} />
             <Route path="/get-involved" element={<GetInvolved />} />
             <Route path="/contact" element={<Contact />} />
+            <Route path="/notice" element={<Notice />} />
             <Route path="/donate" element={<Donate />} />
           </Routes>
         </main>
